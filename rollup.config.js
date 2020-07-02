9c791c56f0e7c5c2669d89162784e296a59e8bef
Spencer Childress <samussiah@gmail.com>
2020-07-02 15:44:24 -0400

    initial commit

---
+++ rollup.config.js
@@ -0,0 +1,32 @@
+import babel from 'rollup-plugin-babel';
+
+const pkg = require('./package.json');
+
+export default {
+    input: pkg.module,
+    output: {
+        name: pkg.name
+            .split('-')
+            .map((str,i) =>
+                i === 0 ?
+                    str :
+                    (str.substring(0,1).toUpperCase() + str.substring(1))
+            )
+            .join(''),
+        file: pkg.main,
+        format: 'umd',
+        globals: {
+            d3: 'd3',
+        },
+    },
+    external: Object.keys(pkg.dependencies),
+    plugins: [
+        babel({
+            exclude: 'node_modules/**',
+            presets: [
+                [ '@babel/preset-env' ]
+            ],
+            babelrc: false
+        })
+    ]
+};

706b4d59c8cd3e0c468fc0d8c1fe5a7ca6f03ac3
Spencer Childress <samussiah@gmail.com>
2020-09-24 17:26:26 -0400

    save changes to countStateChanges.js

--- src/dataManipulation/nestData/countStateChanges.js
+++ src/dataManipulation/nestData/countStateChanges.js
@@ -1,14 +1,14 @@
 export default function countStateChanges(group) {
     const nStateChanges = group
-        //.sort((a,b) => a.start_timepoint - b.start_timepoint)
+        .filter((d) => d.start_timepoint <= this.settings.timepoint)
+        .sort((a, b) => a.start_timepoint - b.start_timepoint)
         .filter((d, i, data) => {
             const eventPrevious = data[i - 1] ? data[i - 1].event : null;
             return (
                 !this.settings.eventChangeCount.includes(eventPrevious) &&
                 this.settings.eventChangeCount.includes(d.event)
             );
-        })
-        .filter((d) => d.start_timepoint <= this.settings.timepoint).length;
+        }).length;
 
     return nStateChanges;
 }

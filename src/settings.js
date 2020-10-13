974e7e13b1be18c27af6d59b80fdfcbaef981679
Spencer Childress <samussiah@gmail.com>
2020-10-12 20:13:41 -0400

    fix #86

--- src/settings.js
+++ src/settings.js
@@ -69,9 +69,9 @@ const settings = {
     modalIndex: 0,
     explanation: [
         'Each bubble in this animation represents an individual.',
-        'As individuals experience events and change states, their bubble gravitates toward the focus representing that event.',
-        'The number of state changes dictates the color and/or size of the bubbles.',
-        'Static bubbles represent individuals whose state never changes.',
+        'As individuals experience events, their bubble gravitates toward the focus or "planet" representing that event.',
+        'The number of events an individual experiences dictates the color and/or size of the bubbles.',
+        'Static bubbles represent individuals who never experience an event.',
         'Use the controls on the right to interact with and alter the animation.',
     ], // array of strings
     information: null, // array of strings

5be4660d4fde25237f25af844393a2772096cbaa
Spencer Childress <samussiah@gmail.com>
2020-10-19 10:55:06 -0400

    add test-page without modal

---
+++ test-page/no-modal/index.js
@@ -0,0 +1,16 @@
+fetch('../data/data_2000_fixed.csv')
+    .then(response => response.text())
+    .then(text => d3.csvParse(text))
+    .then(data => {
+        const fdg = forceDirectedGraph(
+            data,
+            '#container',
+            {
+                eventChangeCount: ['Hospitalization', 'ICU'],
+                eventChangeCountAesthetic: 'both',
+                hideControls: true,
+                explanation: [
+                ],
+            }
+        );
+    });

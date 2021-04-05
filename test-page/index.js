246cfe966765e0a3717fb452ba3b8f226558ebe5
Spencer Childress <samussiah@gmail.com>
2021-04-05 09:41:52 -0400

    add data generator script

--- test-page/index.js
+++ test-page/index.js
@@ -1,4 +1,6 @@
-fetch('./data/data_2000_fixed.csv')
+// TODO: add focus vicinity back in
+// TODO: revisit performance
+fetch('./data/1e5.csv')
     .then(response => response.text())
     .then(text => d3.csvParse(text))
     .then(data => {
@@ -9,6 +11,13 @@ fetch('./data/data_2000_fixed.csv')
                 eventLabel: 'HFrEF events',
                 timeRelative: 'since baseline',
                 drawStaticSeparately: true,
+                shapeBy: {
+                    type: 'categorical',
+                    label: 'Arm',
+                    variable: 'arm'
+                },
+                minRadius: 2,
+                fill: true,
                 //playPause: 'pause',
                 //delay: false,
             }
